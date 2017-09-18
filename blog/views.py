864cb3420fff570fa80a04274c43aca12bfc2419
jcchen0121 <daniel@crpsystem.org>
2017-09-18 18:10:25 +0800

    Modified templates with DTL to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 # https://tutorial.djangogirls.org/en/django_views/
+# https://tutorial.djangogirls.org/en/dynamic_data_in_templates/
 def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
+
